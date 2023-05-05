52779634194581d97829500977c99e2811e165a5
Danial Soleimany <102956315+DanialSoleimany@users.noreply.github.com>
2023-05-05 23:42:19 +0330

    Update Prime-Number.py

--- Prime-Number.py
+++ Prime-Number.py
@@ -1,11 +1,16 @@
-x = int(input("Enter a number:\n"))
-count = 0
-
-for i in range(1, x+1):
-    if(x%i == 0):
-        count += 1
-        
-if(count == 2):
-    print("Prime Number")
+def primeNumber(x):
+    count = 0
+    for i in range(1, x+1):
+        if(x%i == 0):
+            count += 1
+    if(count == 2):
+        return 1 # Prime number
+    else:
+        return 0 # not Prime number
+    
+x = int(input("number: "))
+    
+if(primeNumber(x) == 1):
+    print("{0} is prime number".format(x))
 else:
-    print("Not Prime Number")
+    print("{0} isn't prime number".format(x))
